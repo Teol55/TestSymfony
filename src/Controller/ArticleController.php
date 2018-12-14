3dbc58624a6c43ba0a720b7e3cab23784f028a8a
C_MARTIN <m.ch55000@gmail.com>
2018-12-14 09:36:20 +0100

    ajout du profiler

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,18 +8,28 @@
 
 namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/news")
+     * @Route("/news/{slug}")
      */
 
-    public function homePage()
+    public function homePage($slug)
     {
-        return new Response('OMG! My first page already! WOOO!');
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('/article/show.html.twig',['title' =>ucwords(str_replace('-',' ',$slug)),
+            'comments'=>$comments,
+
+            ]);
+
     }
     /**
      * @Route("/test/{slug}")
