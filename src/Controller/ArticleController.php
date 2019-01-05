0f1ca73db471853a2bf907eca5fff278f4b6de10
C_MARTIN <m.ch55000@gmail.com>
2019-01-05 12:18:53 +0100

    create Comment entity + relation with article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,7 @@
 namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -40,14 +41,10 @@ class ArticleController extends AbstractController
     {
 
 
-        $comments = [
-            'I ate a normal rock once. It did NOT taste like bacon!',
-            'Woohoo! I\'m going on an all-asteroid diet!',
-            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
+
         return $this->render('/article/show.html.twig', [
             'article' => $article,
-            'comments' => $comments,
+
 
         ]);
 
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use App\Entity\Article;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -24,7 +25,7 @@ class ArticleFixtures extends BaseFixtures
     Protected function loadData(ObjectManager $manager)
     {
 
-        $this->createMany(Article::class, 10, function(Article $article, $count) {
+        $this->createMany(Article::class, 10, function(Article $article, $count)use($manager) {
 
             $article->setTitle($this->faker->randomElement(self::$articleTitles))
 
@@ -52,7 +53,17 @@ EOF
             if ($this->faker->boolean(70)) {
                 $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
+            $comment1 = new Comment();
+            $comment1->setAuthorName('Mike Ferengi');
+            $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
+            $comment1->setArticle($article);
+            $manager->persist($comment1);
 
+            $comment2 = new Comment();
+            $comment2->setAuthorName('Mike Ferengi');
+            $comment2->setContent('Woohoo! I\'m going on an all-asteroid diet!');
+            $comment2->setArticle($article);
+            $manager->persist($comment2);
 
 
 
