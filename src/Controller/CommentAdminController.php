a1a5ef467fd2295379ae1a85d56a700e924b65d1
C_MARTIN <m.ch55000@gmail.com>
2019-01-09 12:03:04 +0100

    change view and create CommentAdminContoller

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentAdminController extends AbstractController
 {
     /**
-     * @Route("/comment/admin", name="comment_admin")
+     * @Route("/admin/comment", name="comment_admin")
      */
-    public function index()
+    public function index(CommentRepository $repository)
     {
+        $comments=$repository->findBy([],['createdAt' => 'DESC']);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
