e1c600020be2b452a376a35c0290a0f20f817867
C_MARTIN <m.ch55000@gmail.com>
2019-01-22 15:27:52 +0100

    change fixtures for password user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,15 +4,27 @@ namespace App\DataFixtures;
 use app\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends BaseFixtures
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'main_users', function($i) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'engage'));
             return $user;
         });
         $manager->flush();
